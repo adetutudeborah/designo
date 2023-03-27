ec9b1722bdbe6ad0ec7e3e99974e80cba835b071
Adetutu Oluwasanmi <oluwasanmiadetutu@gmail.com>
2023-03-27 19:04:44 +0100

    added the app design and graphic design page

--- src/Pages/Appdesign/Appdesign.jsx
+++ src/Pages/Appdesign/Appdesign.jsx
@@ -1,12 +1,121 @@
-import React from 'react';
-import './Appdesign.css';
+import React, { useState }  from 'react';
+import '../Webdesign/Webdesign.css';
+import { Link } from 'react-router-dom';
+import Getintouch from '../../Components/Getintouch/Getintouch'; 
 
 function Appdesign() {
+
+    const [projects] = useState([
+        {
+            id:1,
+            title:" Airfilter",
+            description: "Solving the problem of poor indoor air quality by filtering the air",
+            image: "/appdesign/image-airfilter.jpg",
+            alt: "airfilter-image"
+        },
+        {
+            id:2,
+            title: "Eyecam",
+            description: "Product that lets you edit your favorite photos and videos at any time",
+            image: "/appdesign/image-eyecam.jpg",
+            alt: "eyecam-image"
+        },
+        {
+            id:3,
+            title:"Faceit",
+            description: "Get to meet your favorite internet superstar with the faceit app",
+            image: "/appdesign/image-faceit.jpg",
+            alt: "faceit-image"
+        },
+        {
+            id:4,
+            title: "Todo",
+            description: "A todo app that features cloud sync with light and dark mode",
+            image: "/appdesign/image-todo.jpg",
+            alt: "todo-image"
+        },
+        {
+            id:5,
+            title: "Loopstudios",
+            description: "A VR experience app made for Loopstudios",
+            image: "/webdesign/image-blogr.jpg",
+            alt: "blogr-image"
+        }
+    ]);
+
     return(
-        <div className='appdesigncontainer'>
-           App design 
-       </div>
+        <div className='container'>
+
+             {/* Header section */}
+
+                <div className='designheader'>
+                        <h2 className='designtext text-center mb-3'>App Design</h2>
+                        <p className='designsubtext text-center'>  Our mobile designs bring intuitive digital solutions  <br /> to your customers right at their fingertips.</p>
+                </div>
+
+            {/* Web Projects Section */}
+
+                <div className='designprojects'>
+
+                    <div className='row'>
+                     
+                     {projects.map((item) => (
+
+                        <div key={item.id} className='project col-lg-4 mt-5'>
+
+                            <div className='projectimage'>
+                                <img src={item.image} alt={item.alt} className='img-fluid projectimg' />
+                            </div>
+
+                            <div className='projectinfo'>
+                                <h4 className='projecttitle text-center mt-3 mb-3'>{item.title}</h4>
+                                <p className='projectdecription text-center'>{item.description}</p>
+                            </div>
+
+                        </div>
+
+                    ))}
+                        
+                    </div>
+                </div>  
+
+                {/* Other Projects section */}
+
+                <div className='otherprojectcontainer text-center'>
+                    <div className='row '>
+
+                        <div className='col-lg-6 otherproject appproject'>
+                            
+                            <div className='otherprojectinner text-center'>
+                                <p className='projectcategory'>Web design</p>
+                            <Link to="/webdesign">
+                                <p className='viewproject'>View projects <span className='designicon'>&rsaquo;</span></p>
+                            </Link>  
+                            </div>
+                        
+                        </div>
+
+                        <div className='col-lg-6 otherproject graphicproject'>
+
+                            <div className='otherprojectinner text-center'>
+                                <p className='projectcategory'>Graphic design</p>
+                            <Link to="/graphicdesign">
+                                <p className='viewproject'>View projects <span className='projecticon'>&rsaquo;</span></p>
+                            </Link>  
+                            </div>
+                        
+                        </div>
+
+                    </div>
+                </div>
+
+              {/* Let's talk about your project */}
+
+                <div> 
+                    <Getintouch />
+                </div>
+        </div>
     )
 }
 
-export default Appdesign;
+export default Appdesign; 
--- src/Pages/Graphicdesign/Graphicdesign.jsx
+++ src/Pages/Graphicdesign/Graphicdesign.jsx
@@ -1,12 +1,107 @@
-import React from 'react';
-import './Graphicdesign.css';
+import React, { useState }  from 'react';
+import '../Webdesign/Webdesign.css';
+import { Link } from 'react-router-dom';
+import Getintouch from '../../Components/Getintouch/Getintouch'; 
 
 function Graphicdesign() {
+
+    const [projects] = useState([
+        {
+            id:1,
+            title:"Tim Brown",
+            description: "A book cover designed for Tim Brown’s new release, ‘Change’",
+            image: "/graphicdesign/image-change.jpg",
+            alt: "change-image"
+        },
+        {
+            id:2,
+            title: "Boxed water",
+            description: "A simple packaging concept made for Boxed Water",
+            image: "/graphicdesign/image-boxed-water.jpg",
+            alt: "boxed-water-image"
+        },
+        {
+            id:3,
+            title:"Science!",
+            description: "A poster made in collaboration with the Federal Art Project",
+            image: "/graphicdesign/image-science.jpg",
+            alt: "science-image"
+        }
+    ]);
+
     return(
-        <div className='graphicdesigncontainer'>
-          Graphic design
-       </div>
+        <div className='container'>
+
+             {/* Header section */}
+
+                <div className='designheader'>
+                        <h2 className='designtext text-center mb-3'>Graphic design</h2>
+                        <p className='designsubtext text-center'>We deliver eye-catching branding materials that are  <br /> tailored to meet your business objectives.</p>
+                </div>
+
+            {/* Web Projects Section */}
+
+                <div className='designprojects'>
+
+                    <div className='row'>
+                     
+                     {projects.map((item) => (
+
+                        <div key={item.id} className='project col-lg-4 mt-5'>
+
+                            <div className='projectimage'>
+                                <img src={item.image} alt={item.alt} className='img-fluid projectimg' />
+                            </div>
+
+                            <div className='projectinfo'>
+                                <h4 className='projecttitle text-center mt-3 mb-3'>{item.title}</h4>
+                                <p className='projectdecription text-center'>{item.description}</p>
+                            </div>
+
+                        </div>
+
+                    ))}
+                        
+                    </div>
+                </div>  
+
+                {/* Other Projects section */}
+
+                <div className='otherprojectcontainer text-center'>
+                    <div className='row '>
+
+                        <div className='col-lg-6 otherproject appproject'>
+                            
+                            <div className='otherprojectinner text-center'>
+                                <p className='projectcategory'>App design</p>
+                            <Link to="/appdesign">
+                                <p className='viewproject'>View projects <span className='designicon'>&rsaquo;</span></p>
+                            </Link>  
+                            </div>
+                        
+                        </div>
+
+                        <div className='col-lg-6 otherproject graphicproject'>
+
+                            <div className='otherprojectinner text-center'>
+                                <p className='projectcategory'>Web design</p>
+                            <Link to="/webdesign">
+                                <p className='viewproject'>View projects <span className='projecticon'>&rsaquo;</span></p>
+                            </Link>  
+                            </div>
+                        
+                        </div>
+
+                    </div>
+                </div>
+
+              {/* Let's talk about your project */}
+
+                <div> 
+                    <Getintouch />
+                </div>
+        </div>
     )
 }
 
-export default Graphicdesign;
+export default Graphicdesign; 
